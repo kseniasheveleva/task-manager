fd2311286b89a6ca1b30e1f28fa81712aa52c0ed
ksenia sheveleva <kseniashevelevaa@gmail.com>
2024-03-27 21:13:11 +0300

    add router-link

---
+++ src/components/router-link/router-link.component.js
@@ -0,0 +1,34 @@
+import { EVENT_TYPES } from "../../constants/eventTypes";
+import { Component } from "../../core/Component";
+import { eventEmitter } from "../../core/EventEmitter";
+import { useNavigate } from "../../hooks/useNavigate";
+import { template } from "./router-link.template.hbs";
+
+export class RouterLink extends Component {
+    constructor() {
+        super();
+
+        this.template = template();
+        this.state = {
+            href: this.getAttribute("href"),
+            class: this.getAttribute("class"),
+            label: this.getAttribute("label"),
+        };
+    }
+
+    onClick = (evt) => {
+        evt.preventDefault();
+        eventEmitter.emit(EVENT_TYPES.changeRoute, { target: this.state.href })
+        // useNavigate(this.state.href) можно и так
+    }
+
+    componentDidMount() {
+        this.addEventListener('click', this.onClick)
+    }
+
+    componentWillUndMount() {
+        this.removeEventListener('click', this.onClick)
+    }
+}
+
+customElements.define('router-link', RouterLink)
