4475beff488b26eb81801113d13d0ab1fd99ea26
ksenia <kseniashevelevaa@gmail.com>
2024-04-24 17:45:48 +0300

    create task method

--- src/pages/board/board.component.js
+++ src/pages/board/board.component.js
@@ -2,16 +2,15 @@ import { Component } from "../../core/Component";
 import template from "./board.template.hbs";
 import { useUserStore } from "../../hooks/useUserStore";
 import { INITIAL_STATE } from "./initialState";
-import { ROUTES } from "../../constants/routes";
 import { useNavigate } from "../../hooks/useNavigate";
+import { ROUTES } from "../../constants/routes";
 import { useModal } from "../../hooks/useModal";
 import { extractFormData } from "../../utils/extractFormData";
 import { storageService } from "../../services/Storage";
-import { createTaskApi, getTasksApi } from "../../api/tasks";
+import { createTaskAPI, getAllTasksAPI } from "../../api/tasks";
 import { TASK_STATUSES } from "../../constants/task";
+import { useToastNotification } from "../../hooks/useToastNotification";
 import { mapResponseApiData } from "../../utils/api";
-
-
 
 export class BoardPage extends Component {
   constructor() {
@@ -19,13 +18,6 @@ export class BoardPage extends Component {
     this.state = INITIAL_STATE;
     this.template = template();
   }
-
-  toggleIsLoading = () => {
-    this.setState({
-      ...this.state,
-      isLoading: !this.state.isLoading,
-    });
-  };
 
   initialization() {
     const { getUser } = useUserStore();
@@ -36,14 +28,45 @@ export class BoardPage extends Component {
     });
   }
 
-  
+  toggleIsLoading = () => {
+    this.setState({
+      ...this.state,
+      isLoading: !this.state.isLoading,
+    });
+  };
+
+  getAllTasks() {
+    const { user, boardId } = this.state;
+    if (user?.uid) {
+      this.toggleIsLoading();
+
+      getAllTasksAPI(user.uid, boardId)
+        .then(({ data }) => {
+          this.setState({
+            ...this.state,
+            columns: this.state.columns.map((column) => {
+              return {
+                ...column,
+                tasks: mapResponseApiData(data).filter(
+                  (item) => item.status === column.status
+                ),
+              };
+            }),
+          });
+        })
+        .catch(({ message }) => {
+          useToastNotification({ message });
+        })
+        .finally(() => this.toggleIsLoading());
+    }
+  }
 
   uploadAttachments(attachments) {
-    const { boardId, user } = this.state;
+    const { user, boardId } = this.state;
     const path = `${user.uid}/${boardId}`;
     const promiseFiles = attachments.map((attachment) => {
-      return storageService.uploadFile(attachment, path)
-    })
+      return storageService.uploadFile(attachment, path);
+    });
 
     return Promise.all(promiseFiles);
   }
@@ -51,77 +74,64 @@ export class BoardPage extends Component {
   loadAttachmentsUrl(data) {
     return Promise.all(
       data.map((snapshot) => storageService.downloadURL(snapshot.ref))
-    )
+    );
   }
 
-  loadAllTasks() {
-    const { user, boardId } = INITIAL_STATE
-    if (user?.uid) {
-      this.toggleIsLoading();
-      getTasksApi(user.uid, boardId).then(({data}) => {
-        this.setState({
-          ...this.state,
-          columns: this.state.columns.map((column) => {
-            return {
-              ...this.state.columns,
-              tasks: mapResponseApiData(data).filter((task) => task.status === column.status)
-            }
-          })
-        })
-      })
-      .catch(({ message }) => {
-          useToastNotification({ message });
-        })
-      .finally(() => {
+  openCreateTaskModal = () => {
+    useModal({
+      isOpen: true,
+      title: "Create Task",
+      successCaption: "Create",
+      template: "ui-create-task-form",
+      onSuccess: (modal) => {
+        const form = modal.querySelector(".create-task-form");
+        const formData = new FormData(form);
+        const preparedData = {
+          ...extractFormData(form),
+          attachments: formData.getAll("attachments"),
+        };
         this.toggleIsLoading();
-      });
-    }
-  }
+        this.uploadAttachments(preparedData.attachments)
+          .then(this.loadAttachmentsUrl)
+          .then((data) => {
+            createTaskAPI({
+              uid: this.state.user.uid,
+              boardId: this.state.boardId,
+              data: {
+                ...preparedData,
+                attachments: data,
+                status: TASK_STATUSES.todo,
+                createdAt: new Date(),
+              },
+            }).then(() => this.getAllTasks());
+          })
+          .catch(({ message }) => {
+            useToastNotification({ message });
+          })
+          .finally(() => {
+            this.toggleIsLoading();
+          });
+      },
+    });
+  };
 
   onClick = ({ target }) => {
-    const backBtn = target.closest(".go-to-dashboard");
+    const goToDashboard = target.closest(".go-to-dashboard");
     const createTaskBtn = target.closest(".create-task-btn");
 
-    if (backBtn) {
-      useNavigate(ROUTES.dashboard)
+    if (goToDashboard) {
+      useNavigate(ROUTES.dashboard);
     }
 
     if (createTaskBtn) {
-      useModal({
-        isOpen: true,
-        title: "Create Task",
-        successCaption: "Create",
-        template: "ui-create-task-form",
-        onSuccess: (modal) => {
-          const form = modal.querySelector('.create-task-form');
-          const formData = new FormData(form);
-          const prepareData = {
-            ...extractFormData(form),
-            attachments: formData.getAll("attachments") ?? []
-          }
-
-          this.uploadAttachments(prepareData.attachments).then(this.loadAttachmentsUrl)
-          .then((data) => {
-            createTaskApi({
-              id: this.state.user.uid,
-              boardId: this.state.boardId,
-              data: {
-                ...prepareData,
-                attachments: data,
-                status: TASK_STATUSES.todo,
-                createdAt: new Date()
-              },
-            }).then(() => this.loadAllTasks())
-          })
-        }
-      })
+      this.openCreateTaskModal();
     }
-  }
+  };
 
   componentDidMount() {
     this.initialization();
-    this.loadAllTasks();
-    this.addEventListener("click", this.onClick)
+    this.getAllTasks();
+    this.addEventListener("click", this.onClick);
   }
 }
 
